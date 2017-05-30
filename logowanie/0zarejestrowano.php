7309e09068dcea089a107c806b74c030eb291013
DarkMike <osmiornicachaosu@wp.pl>
2017-05-30 09:48:20 +0300

    Update 0zarejestrowano.php

--- logowanie/0zarejestrowano.php
+++ logowanie/0zarejestrowano.php
@@ -13,7 +13,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 $pieprz = "1qaz7UHN";
 $hash = password_hash(mysqli_real_escape_string($conn, $_POST["password"]).$pieprz, PASSWORD_DEFAULT);
-$sql = "INSERT INTO uzytkownik (username, hash) VALUES (\"".mysqli_real_escape_string($conn, $_POST["username"])."\" , \"".$hash."\")";
+$sql = "INSERT INTO uzytkownik (login, hash) VALUES (\"".mysqli_real_escape_string($conn, $_POST["username"])."\" , \"".$hash."\")";
 
 $conn->query($sql);
 
