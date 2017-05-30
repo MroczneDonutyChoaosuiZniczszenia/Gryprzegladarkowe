bf87226e2e5db1bbc02662e56a814c0baa8f5c7b
DarkMike <osmiornicachaosu@wp.pl>
2017-05-30 10:05:26 +0300

    Update rejestracja.php

--- logowanie/rejestracja.php
+++ logowanie/rejestracja.php
@@ -20,7 +20,7 @@ $conn = new mysqli($servername, $username, $password);
 Dzisiaj jest: <?php echo date("d F o");
 ?>
 
-<form action = zarejestrowano.php method = "POST">
+<form action = 'zarejestrowano.php' method = "POST">
 	Nazwa uzytkownika: <input type = "text" name = "username"/><br/>
 	Haslo: <input type = "password" name = "password"/><br/>
 	<input type = "submit" value = "zarejestruj"/><br/>
