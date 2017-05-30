51995944ccb04d139e38ee7644614b0444ef442f
DarkMike <osmiornicachaosu@wp.pl>
2017-05-30 10:05:05 +0300

    Update logowanie.php

--- logowanie/logowanie.php
+++ logowanie/logowanie.php
@@ -16,7 +16,7 @@ $dbname = "uzytkowncy";
 $conn = new mysqli($servername, $username, $password);
 
 ?>
-<form action = zalogowano.php method = "POST">
+<form action = 'zalogowano.php' method = "POST">
 	Nazwa uzytkownika: <input type = "text" name = "username"/><br/>
 	Haslo: <input type = "password" name = "password"/><br/>
 	<input type = "submit" value = "Zaloguj"/><br/>
