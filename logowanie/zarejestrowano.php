6e456996d479ef67307342269a214c50356444c0
DarkMike <osmiornicachaosu@wp.pl>
2017-05-31 13:47:34 +0200

    update
    próba rozwiązania niezidentyfikowanych problemów z bazą danych i rejestracją

--- logowanie/zarejestrowano.php
+++ logowanie/zarejestrowano.php
@@ -11,10 +11,11 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 $pieprz = "1qaz7UHN";
 $hash = password_hash(mysqli_real_escape_string($conn, $_POST["password"]).$pieprz, PASSWORD_DEFAULT);
+echo "<p>".$hash."</p>";
 $sql = "INSERT INTO uzytkownik (login, hash) VALUES (\"".mysqli_real_escape_string($conn, $_POST["username"])."\" , \"".$hash."\")";
-
+echo "<p>".$sql."</p>";
 $conn->query($sql);
-
+echo "<p>".$conn->error."</p>";
 ?>
 zarejestrowano!
 <a href="logowanie.php">zaloguj</a>
